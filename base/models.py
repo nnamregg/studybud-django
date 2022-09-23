d40dc0bdce71539c74416e2f29eec97295fbb44e
nnamregg <nnamreg.t@gmail.com>
2022-09-23 13:34:13 -0300

    Arreglo error en modelo de usuario

--- base/models.py
+++ base/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
@@ -33,8 +32,8 @@ class Room(models.Model):
         return self.name
     
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
-    room = models.ForeignKey(Room, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
